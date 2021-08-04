6e3d95331653b81a67deb86555d3b0894bd6c770
l0x539 <nogues322@gmail.com>
2021-08-04 05:08:46 +0000

    Beta release stable.

--- config/database.js
+++ config/database.js
@@ -10,8 +10,8 @@ module.exports = ({ env }) => ({
         host: env('DATABASE_HOST', 'localhost'),
         port: env.int('DATABASE_PORT', 3306),
         database: env('DATABASE_NAME', 'sahla'),
-        username: env('DATABASE_USERNAME', 'root'),
-        password: env('DATABASE_PASSWORD', '984156327'),
+        username: env('DATABASE_USERNAME', 'nico'),
+        password: env('DATABASE_PASSWORD', 'gUs_*bHdH9a^5awe'),
       },
       options: {
         useNullAsDefault: true,
